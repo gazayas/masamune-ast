927d8c4a70f8c93122b049a653430265eab7d56e
Gabriel Zayas <g-zayas@hotmail.com>
2023-06-02 14:40:27 +0900

    Add the sorting method for position and token arrays

--- lib/masamune-ast/abstract_syntax_tree/data_node.rb
+++ lib/masamune-ast/abstract_syntax_tree/data_node.rb
@@ -3,6 +3,14 @@
 # These values are the `type`, `token`, and `position`, respectively.
 # It is simliar to what you see in `Ripper.lex(code)` and `Masamune::AbstractSyntaxTree's @lex_nodes`.
 
+# We break this down into a simpler structure, `position_and_token`,
+# which looks like this: [[4, 7], "ruby"]
+# TODO: I eventually want to change this to the following:
+# [
+#   {position: [4, 7], token: "ruby"},
+#   {position: [1, 2], token: "rails"}
+# ]
+
 module Masamune
   class AbstractSyntaxTree
     class DataNode < Node
@@ -13,15 +21,42 @@ module Masamune
         super(contents, ast_id)
       end
 
-      def position_and_token
-        [@line_position, @token]
-      end
-
-      # TODO: Write this method.
       # Results here represent the data we get when searching for data.
       # For example, [[[4, 7], "ruby"], [[7, 7], "rails"]].
+      # TODO: Worry about using a faster sorting algorithm later.
       def self.order_results_by_position(position_and_token_ary)
-        position_and_token_ary
+        line_numbers = position_and_token_ary.map do |position_and_token|
+          position = position_and_token.first
+          line_number = position.first
+          line_number
+        end.uniq.sort
+
+        final_result = []
+        line_numbers.each do |line_number|
+          # Group data together in an array if they're on the same line.
+          shared_line_data = position_and_token_ary.select do |position_and_token|
+            position = position_and_token.first
+            position.first == line_number
+          end
+
+          # Sort the positions on each line number respectively.
+          positions_on_line = shared_line_data.map do |data|
+            data.first.last
+          end.sort
+
+          # Apply to the final result.
+          positions_on_line.each do |position|
+            shared_line_data.each do |data|
+              final_result << data if data.first.last == position
+            end
+          end
+        end
+
+        final_result
+      end
+
+      def position_and_token
+        [@line_position, @token]
       end
     end
   end
