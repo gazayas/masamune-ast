d3e88646d3312a8748ce6733ad19f3f2c5cb1c69
Gabriel Zayas <g-zayas@hotmail.com>
2023-06-07 14:06:43 +0900

    Extract params method to block support node

--- lib/masamune/abstract_syntax_tree/nodes/blocks/brace_block.rb
+++ lib/masamune/abstract_syntax_tree/nodes/blocks/brace_block.rb
@@ -2,17 +2,11 @@
 
 module Masamune
   class AbstractSyntaxTree
-    class BraceBlock < Node
+    class BraceBlock < Block
       attr_accessor :ast_id
 
       def initialize(contents, ast_id)
         super
-      end
-
-      def params
-        # This node should exist already, so we search for it in the ast object.
-        block_var = BlockVar.new(contents[1], ast_id)
-        ast.node_list.find {|node| node.contents == block_var.contents}
       end
     end
   end
